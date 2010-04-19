34923aac72619e670dacfee2f7aa33d5c3bde723
Matheus E. Muller <hello@memuller.com>
2010-04-18 23:53:39 -0300

    Added a functional Listable module that makes media Items searchable.

--- lib/claire_client/listable.rb
+++ lib/claire_client/listable.rb
@@ -1,19 +1,26 @@
 module Claire
 	module Client
 		module Listable
-			
-			
-			
-			
-			def Listable.included base
-				base.extend ClassMethods
-			end
-						
+					 						
 			module ClassMethods
-				def all
-					body = get self.base_url
-					List.new()					
+				
+				# Returns a Claire::List on its base_url.
+				def all					
+					List.new(self.base_url)					
 				end
+				
+			end
+			
+			# Extends the class methods above.
+			def Listable.included base
+				raise Error, "only Claire::Client::Item s can be Listable" unless Item.children.include? base
+				base.extend ClassMethods
+				(@@children ||= []) << base
+			end
+			
+			# Lists all avaliable Claire::Items			
+			def self.children
+				@@children ||= []
 			end
 			
 		end
--- spec/claire_client/listable_spec.rb
+++ spec/claire_client/listable_spec.rb
@@ -1,1 +1,44 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+class Video
+	include Claire::Client::Item
+	include Claire::Client::Listable	
+end
+
+describe Claire::Client::Listable do
+  before do
+    Claire::Client.stub(:get).with('videos').and_return xml(:item_with_channel)
+		Claire::Client.stub(:get).with('videos/id').and_return xml(:item)
+  end
+
+	describe "initialization" do
+		it "should work only on Claire::Items" do
+		  lambda { eval("
+				class NotAVideo
+					include Claire::Client::Listable
+				end
+			") }.should raise_error Claire::Error
+			lambda { Video.new('videos/id')}.should_not raise_error
+		end				
+	end
+	
+	describe "its all method" do
+		it "should be defined" do
+			Video.should respond_to :all
+		end
+		
+		it "should return a Claire::List on the item's base URL" do
+		  Video.all.should be_a_kind_of Claire::Client::List
+		end
+	end
+	
+	it "delegate a count property to its all method" # do
+	 # 		Video.count.should == 2
+	 # 	end                                             
+	
+	it "should have a list of its children" do
+	  Claire::Client::Listable.children.include?(Video).should be_true
+	end
+	
+
+end
